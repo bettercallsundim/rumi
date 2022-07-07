23f85905e3aeef9ff22f6b4f599a6014ce85f647
meandme <sunmaxboom2@gmail.coml>
2022-07-07 12:20:50 +0600

    ok

---
+++ app.js
@@ -0,0 +1,13 @@
+onePageScroll(".main", {
+   sectionContainer: "section",
+   easing: "ease",
+   animationTime: 500,
+   pagination: true,
+   updateURL: false,
+   keyboard: true,
+   beforeMove: null,
+   afterMove: null,
+   loop: true,
+   responsiveFallback: false,
+
+   });
